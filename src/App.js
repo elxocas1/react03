b035e7f70689bd9c79c6ef923ae339691a3dd9db
Elvis20-23 <kiubyes.15@gmail.com>
2023-10-20 09:17:01 -0500

    v6. Instalo fb: auth, storage, firestore

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
     <div style={{background:"blue", width:"350px",padding:"10px"}}>
       <AppForm />
       <i class="material-icons">insert_chart</i>
+
+      <p>1. Juan Manuel 23 Masculino  ---- x -  A</p>
+      <p>2. Rosa Maria  25 Femenino   ---- x -  A</p>
+      <p>3. Luis Miguel 40 Masculino  ---- x -  A</p>
+      
     </div>
   );
 }
---
+++ src/firebase/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import { getAuth } from "firebase/auth";
+import { getStorage, ref, uploadBytes } from "firebase/storage"
+import { getFirestore, addDoc, collection, getDoc, getDocs, query, where, setDoc, deleteDoc, updateDoc } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbBAKhPT9sNTmZBZy6GbT3ajP2q20p5mU",
+  authDomain: "elxocas03-6886c.firebaseapp.com",
+  projectId: "elxocas03-6886c",
+  storageBucket: "elxocas03-6886c.appspot.com",
+  messagingSenderId: "434402923773",
+  appId: "1:434402923773:web:bf9d6cb3ab0fbf156a1a94",
+  measurementId: "G-85RR4RJGRN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth      = getAuth(app);
+export const db = getFirestore(App);
+const storage   = getStorage(App);
