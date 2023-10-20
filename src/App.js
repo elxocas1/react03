5b10d92e5147d66361fdad5d1d9425027c9a3083
elxocas <fabianpachao20@gmail.com>
2023-10-20 00:42:01 -0600

    19/10/23

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import C01componente from './components/C01componente';
-import Variable from './components/P4variable';
+//import C01componente from './components/C01componente';
+//import Variable from './components/C04variable';
+import AppForm from './components/AppForm';
+//import MatrizOperaciones from './components/P7matrizOperaciones';
+
 
 function App() {
   return (
-    <div>
-      <C01componente/>
-      <Variable xVariable />
+    <div style={{background:"blue", width:"350px",padding:"10px"}}>
+      <AppForm />
+      <i class="material-icons">insert_chart</i>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/AppForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Vari= (msj) => {
+  return (
+    <div style={{background:"green",padding:"10px", textAlign:"center"}}>
+      <h>AppForm.js</h><br/>
+      <input placeholder='Nombres...' /><br/>
+      <input placeholder='Edad...' /><br/>
+      <input placeholder='Genero...' /><br/>
+      <button>Guardar</button>
+    </div>
+  )
+}
+
+export default Vari;
